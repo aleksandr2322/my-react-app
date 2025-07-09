0e2b6f2848823807fac8ad85a3ce660f081561f5
aleksandr2322 <pavlovskiy28201@gmail.com>
2025-07-09 11:17:39 +0000

    gdrfgr

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,13 +5,13 @@ const Footer: React.FC = () => (
   <footer className={styles.footer}>
     <div className={styles.foot_all}>
       <div className={styles.footer__right}>
-        <p className={styles.footer__text}>Jobly.co</p>
-        <p className={styles.footer__text}>We support a pool of diverse young creatives and engineers.</p>
-        <p className={styles.footer__text}>© 2023 Jobly.co Copyright and All rights reserved.</p>
+        <p className={styles.footer__text}>talentmagnet.com</p>
+        <p className={styles.footer__text}>Мы поддерживаем команду самых разных молодых креативщиков и инженеров.</p>
+        <p className={styles.footer__text}>© 2023 talentmagnet.com Авторское право и все остальные права защищены.</p>
       </div>
       <div className={styles.foot_left}>
-        <a className={styles.footer__link} href="#terms">Terms and Conditions</a> · 
-        <a className={styles.footer__link} href="#privacy">Privacy Policy</a>
+        <a className={styles.footer__link} href="#terms">Условия и положения</a> · 
+        <a className={styles.footer__link} href="#privacy">Политика конфиденциальности</a>
       </div>
     </div>
   </footer>
--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,14 +1,4 @@
-// import React from "react";
-// import styles from "../../assets/styles/Preloader.module.css";
 
-
-// const Preloader: React.FC = () => (
-//   <div className={styles.preloader}>
-//     <div className={styles.loader}></div>
-//   </div>
-// );
-
-// export default Preloader;
 import React, { useEffect } from "react";
 import styles from "../../assets/styles/Preloader.module.css";
 
